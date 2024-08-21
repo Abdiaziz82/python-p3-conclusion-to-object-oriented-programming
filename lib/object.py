8505c09bbbd7477d9aec3cd213022ec990ef5fe1
Abdiaziz82 <abdiazizhared64@gmail.com>
2024-08-21 11:33:55 +0300

    initial commit

---
+++ lib/object.py
@@ -0,0 +1,10 @@
+class MyObject:
+    def __init__(self, size, height):
+        self.size = size
+        self.height = height
+
+# Create an instance of MyObject
+animal = MyObject(15, 14)
+
+# Print the instance
+print(animal)
